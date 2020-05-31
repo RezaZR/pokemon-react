55c0896fe1f817041ca360fd6170a5c223cfa3bc
RezaZR <rezazramadhan@gmail.com>
2020-05-31 21:22:28 +0700

    build project

---
+++ build/precache-manifest.b1ab064387fe9198019612d9a1c06afc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "655e307ef6d5b8dd5531c187c7e7cd41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11e78c5950d036dbf713",
+    "url": "/static/css/main.5d2e1050.chunk.css"
+  },
+  {
+    "revision": "6b7c453b96c7eb4b6017",
+    "url": "/static/js/2.84d42da6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84d42da6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11e78c5950d036dbf713",
+    "url": "/static/js/main.746487b5.chunk.js"
+  },
+  {
+    "revision": "c545bc295dbcc60d2781",
+    "url": "/static/js/runtime-main.7a5c33b1.js"
+  },
+  {
+    "revision": "6b4ffa55fdd7e087c6990685887572b4",
+    "url": "/static/media/pokemon.6b4ffa55.svg"
+  }
+]);
